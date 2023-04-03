338906c5025b0edb4e7d602188673d6a61214966
kalpit10 <kalpit.swami@gmail.com>
2023-04-03 12:21:11 +0530

    Changed some cors code

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use("/api/user/", userRoute);
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update with appropriate domain name
+  res.header("Access-Control-Allow-Origin", "https://resumaid.herokuapp.com"); // update with appropriate domain name
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
