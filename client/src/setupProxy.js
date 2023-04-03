04dd30cc7aa09454d4cd0b4ee97756ccae656146
kalpit10 <kalpit.swami@gmail.com>
2023-04-03 12:25:51 +0530

    CHanged

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
   app.use(
     "/result",
     createProxyMiddleware({
-      target: "https://resumaid.herokuapp.com/",
+      target: "https://resumaid.herokuapp.com",
       changeOrigin: true,
     })
   );
